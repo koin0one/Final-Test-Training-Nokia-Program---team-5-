a486457829df2868685a0a6d3e706e9ea997ac86
duyennguyen <ngduyen2605@gmail.com>
2023-11-30 00:02:22 +0700

    config route

--- Front-end/src/routes/PublicRoute.jsx
+++ Front-end/src/routes/PublicRoute.jsx
@@ -1,10 +1,10 @@
-import { Navigate } from 'react-router-dom'
+// import { Navigate } from 'react-router-dom'
 
-const PublicRoute = ({ children }) => {
-  return <>{children}</>
-  if (window.localStorage.getItem('isLoggedIn')) {
-    return <Navigate to='/' replace />
-  } else return <>{children}</>
-}
+// const PublicRoute = ({ children }) => {
+//   return <>{children}</>
+//   if (window.localStorage.getItem('isLoggedIn')) {
+//     return <Navigate to='/' replace />
+//   } else return <>{children}</>
+// }
 
-export default PublicRoute
+// export default PublicRoute
